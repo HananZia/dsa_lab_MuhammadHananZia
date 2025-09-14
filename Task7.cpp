d86a238e3324800240f6f34fb831bdca4aedd9db
Hanan Zia <hananzia477@gmail.com>
2025-09-15 02:31:17 +0500

    Added Lab Tasks of lab 02

---
+++ Task7.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>   //Importing Vector Function to handle Vector queries
+using namespace std;
+
+//Vector Function to findAllIndices
+vector<int> findAllIndices(const vector<int>& arr, int key) {
+    vector<int> indices;
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == key) {
+            indices.push_back(i);  //Push_back will add indices in vectors
+        }
+    }
+    return indices;
+}
+
+// Void function to print the array
+void printVector(const vector<int>& v) {
+    if (v.empty()) {
+        cout << "No occurrences are found yet." << endl;
+        return;
+    }
+    cout << "Indices: ";
+    for (int idx : v) {
+        cout << idx << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+
+    vector<int> arr1 = {4,7,2,7,9};
+    cout << "Multiple Occurences (Test 01):" << endl;   //Test 01
+    printVector(findAllIndices(arr1, 7)); //Function call
+
+    vector<int> arr2 = {10, 20, 30};
+    cout << "Key not present (Test 02):" << endl;   //Test 02
+    printVector(findAllIndices(arr2, 99));   //Searching 99 with function call
+
+    vector<int> arr3 = {};
+    cout << "Empty array (Test 03):" << endl;  //Test 03
+    printVector(findAllIndices(arr3, 5));   //Function call to find element in empty array
+
+    return 0;
+}
---
+++ Task8.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int findFirstOccurrence(const string& pat, const string& txt) {
+    int M = pat.size();
+    int N = txt.size();
+
+    // empty pattern
+    if (M == 0) return 0;
+
+    // Naive search
+    for (int i = 0; i <= N - M; i++) {
+        int j;
+        for (j = 0; j < M; j++) {
+            if (txt[i + j] != pat[j]) {
+                break;
+            }
+        }
+        if (j == M) {
+            return i; // return immediately on first match
+        }
+    }
+    return -1; // return -1 on no match
+}
+
+int main() {
+    // Test cases
+    cout << "Pattern at beginning: "
+         << findFirstOccurrence("AABA", "AABAACAADAABAABA") << endl; // 0
+
+    cout << "Pattern at end: "
+         << findFirstOccurrence("XYZ", "ABAXYZ") << endl; // 3
+
+    cout << "Pattern not present: "
+         << findFirstOccurrence("TEST", "WELCOME TO NUST") << endl; // -1
+
+    cout << "Empty pattern: "
+         << findFirstOccurrence("", "SOMETHINGWOULDBE") << endl; // 0
+
+    return 0;
+}
